3dd3c40d96a8f88256fee2c11fc6d60bc90eac25
RC_Student_lab <ST10470414@rcconnect.edu.za>
2025-08-13 12:53:04 +0000

    test

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+Scanner scanner= new Scanner(System.in.toString());
+
+        String [] cities = {"JHB","DBN","CTN","PE"};
+        String [] months = {"JAN","FEB","MAR"};
+
+String [][] numbers = {{"128km","135km","139km"},
+        {"155km","129km","175km"},
+        {"129km","130km","185km"},
+        {"195km","155km","221km"}};
+
+System.out.println("enter the city");
+System.out.println("enter the month");
+System.out.println("enter the km");
+
+  System.out.println("******************");
+  System.out.println("Speeding fines report");
+System.out.println("******************");
+
+System.out.println("******************");
+System.out.println("Speeding fines statistics");
+System.out.println("******************");
+
+System.out.println("Maxium speed captured = 221km");
+        System.out.println("Minimum speed captured = 128km");
+        System.out.println("******************");
+
+
+    }
+    }
